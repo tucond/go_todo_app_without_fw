51a906505e0c8887fb2ba5d8f82088cb552d07bf
tucond <kikkikukkuku@gmail.com>
2022-08-07 07:40:28 +0900

    Update to use any port number

--- main.go
+++ main.go
@@ -4,18 +4,33 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Printf("need port number¥n")
+		os.Exit(1)
+	}
+
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)s
+
+	if err != nil {
+		log.Fatalf("failed to listen port:%s, %v", p, err)
+	}
+
+	if err := run(context.Background(), l); err != nil {
 		log.Printf("failed tto terminate server: %v", err)
+		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 
 	s := &http.Server{
 		Addr: ":18080",
@@ -27,7 +42,7 @@ func run(ctx context.Context) error {
 
 	//別ゴルーチンでHTTPサーバを起動する
 	eg.Go(func() error {
-		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
 		}
