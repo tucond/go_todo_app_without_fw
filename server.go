d702cd23ffc3c8de7497ca8cf70786cd7f7babb3
tucond <kikkikukkuku@gmail.com>
2022-08-21 07:27:30 +0900

    Update to use run function in server wrapper

--- server.go
+++ server.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.org/x/sync/errgroup"
 )
 
 type Server struct {
@@ -15,4 +22,25 @@ func NewServer(l net.Listener, mux http.Handler) *Server {
 		srv: &http.Server{Handler: mux},
 		l:   l,
 	}
+}
+
+func (s *Server) Run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if err := s.srv.Serve(s, l); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+	if err := s.srv.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	return eg.Wait()
 }
