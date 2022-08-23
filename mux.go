30fffb8232e008b6fe606945ccd5f7cab998db0b
tucond <kikkikukkuku@gmail.com>
2022-08-24 08:31:03 +0900

    Add mux test

--- mux.go
+++ mux.go
@@ -2,8 +2,8 @@ package main
 
 import "net/http"
 
-func NewMux() http.handler {
-	mux := http.NewSer()
+func NewMux() http.Handler {
+	mux := http.NewServeMux()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
