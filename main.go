a38d8cd09217e9543c922fe0f6692f1727f9f665
tucond <kikkikukkuku@gmail.com>
2022-08-25 08:51:20 +0900

    Use NewMux and NewServer in main function

--- main.go
+++ main.go
@@ -24,9 +24,7 @@ func main() {
 
 func run(ctx context.Context) error {
 
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
-	cfg, err := config.New()
+	cfg, err = config.New()
 	if err != nil {
 		return err
 	}
@@ -35,31 +33,10 @@ func run(ctx context.Context) error {
 	if err != nil {
 		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
+
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
-
-	s := &http.Server{
-		Addr: ":18080",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			//実験用
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-
-	//別ゴルーチンでHTTPサーバを起動する
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
