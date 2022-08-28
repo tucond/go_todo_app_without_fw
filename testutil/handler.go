2bcb41960440d1fbd24dcb6529d5b92662dbae4a
tucond <kikkikukkuku@gmail.com>
2022-08-29 08:01:29 +0900

    Add JSON writing helper function for test

---
+++ testutil/handler.go
@@ -0,0 +1,41 @@
+package testutil
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func AssertJSON(t *testing.T, want, got []byte) {
+	t.Helper()
+
+	var jw, jg any
+	if err := json.Unmarshal(want, &jw); err != nil {
+		t.Fatalf("cannnot unmarshal want %q: %v", want, err)
+	}
+	if diff := cmp.Diff(jg, jw); diff != "" {
+		t.Errorf("got differs: (-got +want)¥n%s", diff)
+	}
+}
+
+func AssertResponse(t *testing.T, got *http.Response, status int, body []byte) {
+	t.Helper()
+	t.Cleanup(func() { _ = got.Body.Close() })
+	gb, err := io.ReadAll(got.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got.StatusCode != status {
+		t.Fatalf("want status %d, but got %d, body: %q", status, got.StatusCode, gb)
+	}
+
+	if len(gb) == 0 && len(body) == 0 {
+		return
+	}
+
+	AssertJSON(t, body, gb)
+}
