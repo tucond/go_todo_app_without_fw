c0f3577c6b07e7d36a45d1b5d0e3f33d91b879da
tucond <kikkikukkuku@gmail.com>
2022-09-03 09:54:53 +0900

    Add http handler test

--- handler/add_task_test.go
+++ handler/add_task_test.go
@@ -1,21 +1,43 @@
 package handler
 
 import (
-	"bytes"
 	"net/http"
-	"net/http/httptest"
 	"testing"
-
 )
 
-func TestAddTask(t *testing.T){
+func TestAddTask(t *testing.T) {
 	t.Parallel()
 	type want struct {
-		status int
+		status  int
 		rspFile string
 	}
-	tests := map[string]struct{
+	tests := map[string]struct {
 		reqFile string
-		want want
-	}//途中
+		want    want
+	}{
+		"ok": {
+			reqFile: "testdata/add_task/ok_req.json.golden",
+			want: want{
+				status:  http.StatusOK,
+				rspFile: "testdata/add_task/ok_rsp.json.golden",
+			},
+		},
+		"badReqest": {
+			reqFile: "testdata/add_task/bad_req.json.golden",
+			want: want{
+				status:  http.StatusBadRequest,
+				rspFile: "testdata/add_task/bad_req_rsp.json.golden",
+			},
+		},
+	}
+
+	for n, tt := range tests {
+		tt := tt
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+
+			//途中
+
+		})
+	}
 }
