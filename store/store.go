75eac9224073a87520a492ad46b9f8b3c85bf7e4
tucond <kikkikukkuku@gmail.com>
2022-09-09 08:39:16 +0900

    Fixed build error

--- store/store.go
+++ store/store.go
@@ -1,13 +1,13 @@
 package store
 
-import(
+import (
 	"errors"
 
 	"github.com/tucond/go_todo_app_without_fw/entity"
 )
 
 var (
-	Tasks = &TaskStore{Tasks: map[int]*entity.Task}
+	Tasks = &TaskStore{Tasks: map[int]*entity.Task{}}
 
 	ErrNotFound = errors.New("not found")
 )
@@ -15,19 +15,19 @@ var (
 type TaskStore struct {
 	//仮実装
 	LastID entity.TaskID
-	Tasks map[entity.TaskID]*entity.Task
+	Tasks  map[entity.TaskID]*entity.Task
 }
 
-func (ts *TaskStore)Add(t *entity.Task)(int, error){
-	ts.LastD++
+func (ts *TaskStore) Add(t *entity.Task) (int, error) {
+	ts.LastID++
 	t.ID = ts.LastID
 	ts.Tasks[t.ID] = t
 	return t.ID, nil
 }
 
-func(ts *TaskStore) All() entity.Tasks{
+func (ts *TaskStore) All() entity.Tasks {
 	tasks := make([]*entity.Task, len(ts.Tasks))
-	for i, t := range ts.Tasks{
+	for i, t := range ts.Tasks {
 		tasks[i-1] = t
 	}
 	return tasks
