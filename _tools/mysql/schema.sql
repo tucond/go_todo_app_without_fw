f41bc67bbbef5f5d1dce71eb8458b4e9097074d7
tucond <kikkikukkuku@gmail.com>
2022-09-12 08:24:51 +0900

    Update sql schema

--- _tools/mysql/schema.sql
+++ _tools/mysql/schema.sql
@@ -1,7 +1,14 @@
 CREATE TABLE `user`
 {
-
-}
+    `id`    BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ユーザーの識別子',
+    `name`  VARCHAR(20) NOT NULL COMMENT 'ユーザー名',
+    `password` VARCHAR(80) NOT NULL COMMENT 'パスワードハッシュ',
+    `role`  VARCHAR NOT NULL COMMENT 'ロール',
+    `created`   DATETIME(6) NOT NULL COMMENT 'レコード作成日時',
+    `modified`  DATETIME(6) NOT NULL COMMENT 'レコード修正日時',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uix_name` (`name`) USING BTREE
+}Engine=InnoDB DEFAULT CHARSET='ユーザー';
 
 CREATE TABLE `task`
 {
