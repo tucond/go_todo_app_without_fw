a4707808e3bc547ee3f3320d9f4d9ded7137b0aa
tucond <kikkikukkuku@gmail.com>
2022-09-14 08:41:40 +0900

    Add sql

--- _tools/mysql/schema.sql
+++ _tools/mysql/schema.sql
@@ -12,5 +12,10 @@ CREATE TABLE `user`
 
 CREATE TABLE `task`
 {
-    
-}
+    `id`    BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'タスクの識別子',
+    `title` VARCHAR(128) NOT NULL COMMENT 'タスクのタイトル'
+    `status`    VARCHAR(20) NOT NULL COMMENT 'タスクの状態'
+    `created`   DATETIME(6) NOT NULL COMMENT 'レコード作成日時'
+    `modified`  DATETIME(6) NOT NULL COMMENT 'レコード修正日時'
+    PRIMARY KEY(`id`)
+}Engine=InnoDB DEFAULT CHARSET='タスク';
