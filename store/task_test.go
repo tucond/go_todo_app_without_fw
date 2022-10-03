2001513ff7feb5bf3713e686e18b617193b9ea20
tucond <kikkikukkuku@gmail.com>
2022-10-04 07:07:27 +0900

    Add RDBMS test

--- store/task_test.go
+++ store/task_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/tucond/go_todo_app_without_fw/clock"
+	"github.com/tucond/go_todo_app_without_fw/entity"
 )
 
 func TestRepository_ListTasks(t *testing.T) {
@@ -24,5 +26,20 @@ func TestRepository_ListTasks(t *testing.T) {
 	}
 	if d := cmp.Diff(gots, wants); len(d) != 0 {
 		t.Error("difers: (-got +want)¥n%s", d)
+	}
+}
+
+func prepareTasks(ctx context.Context, t *testing.T, con Execer) entity.Tasks {
+	t.Helper()
+	if _, err := con.ExecContext(ctx, "DELETE FROM task;"); err != nl {
+		t.Logf("failed to initialize task: %v", err)
+	}
+
+	c := clock.FixedClocker{}
+	wants := entity.Tasks{
+		{
+			Title: "want task 1", Status: "todo",
+			Created: c.Now(), Modified: c.Now(),
+		},
 	}
 }
